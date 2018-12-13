8ec9601ffc99664a9b2d318fac8f9527878fba6f
Tola's Terminal <sohbaker@gmail.com>
2018-12-13 16:24:10 +0000

    new test passed: forEach method return each coin in the array

--- __tests__/coinchanger_tests.js
+++ __tests__/coinchanger_tests.js
@@ -38,6 +38,10 @@ describe('coinchanger', () => {
     expect(coinchanger.changeCoins(200)).toEqual(200);
     });
 
+  [200, 100, 50, 20, 10, 5, 2, 1].forEach(coin => {
+    expect(coinchanger.changeCoins(coin)).toEqual(coin);
+    });
+
   it('returns 2, 1  when given 3', () => {
     expect(coinchanger.changeCoins(3)).toEqual([2, 1]);
     });
