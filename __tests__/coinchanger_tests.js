8e08aed45a29623ac42ec78c3cc39c59523140a1
Tola's Terminal <sohbaker@gmail.com>
2018-12-14 10:46:34 +0000

    new test passed: returns 2, 2 when given 4, plus refactoring

--- __tests__/coinchanger_tests.js
+++ __tests__/coinchanger_tests.js
@@ -2,16 +2,13 @@ import * as coinchanger from '../src/coinchanger';
 
 describe('coinchanger', () => {
   describe('changeCoins', () => {
-    [200, 100, 50, 20, 10, 5, 2, 1].forEach(coin => {
-      expect(coinchanger.changeCoins(coin)).toEqual(coin);
+    it('returns 2, 2 when given 4', () => {
+      expect(coinchanger.changeCoins(4)).toEqual([2, 2]);
       });
 
-    it('returns 2, 1  when given 3', () => {
+    it('returns 2, 1 when given 3', () => {
       expect(coinchanger.changeCoins(3)).toEqual([2, 1]);
       });
 
-    it('returns "not a coin" when given 6', () => {
-      expect(coinchanger.changeCoins(6)).toEqual('not a coin');
-      });
   });
 });
--- src/coinchanger.js
+++ src/coinchanger.js
@@ -1,20 +1,13 @@
 function changeCoins(money) {
-  const possibleChange = [200, 100, 50, 20, 10, 5, 2, 1];
+  const coins = [200, 100, 50, 20, 10, 5, 2, 1];
+  const giveChange = []
 
-  for(let i = 0; i < possibleChange.length; i++) {
-    if(possibleChange[i] === money) {
-      return money;
+  coins.forEach(coin => {
+    while (money >= coin) {
+      giveChange.push(coin);
+      money -= coin;
     }
-  }
-  
-  if(money === 3) {
-    return [2, 1];
-  } else {
-    return('not a coin');
-  }
-
-
-
+  });
+  return giveChange;
 };
-
 module.exports = { changeCoins };
