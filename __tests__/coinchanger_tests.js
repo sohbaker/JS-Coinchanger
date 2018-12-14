72a7ba2532a86c84b3144efcbb075110e7fb4dbe
Tola's Terminal <sohbaker@gmail.com>
2018-12-14 10:13:12 +0000

    new test passed: returns not a coin when given a number which isn't 3 or a coin in the array

--- __tests__/coinchanger_tests.js
+++ __tests__/coinchanger_tests.js
@@ -2,48 +2,16 @@ import * as coinchanger from '../src/coinchanger';
 
 describe('coinchanger', () => {
   describe('changeCoins', () => {
-  it('returns 0 when given 0', () => {
-    expect(coinchanger.changeCoins(0)).toEqual(0);
-    });
+    [200, 100, 50, 20, 10, 5, 2, 1].forEach(coin => {
+      expect(coinchanger.changeCoins(coin)).toEqual(coin);
+      });
 
-  it('returns 1 when given 1', () => {
-    expect(coinchanger.changeCoins(1)).toEqual(1);
-    });
+    it('returns 2, 1  when given 3', () => {
+      expect(coinchanger.changeCoins(3)).toEqual([2, 1]);
+      });
 
-  it('returns 2 when given 2', () => {
-    expect(coinchanger.changeCoins(2)).toEqual(2);
-    });
-
-  it('returns 5 when given 5', () => {
-    expect(coinchanger.changeCoins(5)).toEqual(5);
-    });
-
-  it('returns 10 when given 10', () => {
-    expect(coinchanger.changeCoins(10)).toEqual(10);
-    });
-
-  it('returns 20 when given 20', () => {
-    expect(coinchanger.changeCoins(20)).toEqual(20);
-    });
-
-  it('returns 50 when given 50', () => {
-    expect(coinchanger.changeCoins(50)).toEqual(50);
-    });
-
-  it('returns 100 when given 100', () => {
-    expect(coinchanger.changeCoins(100)).toEqual(100);
-    });
-
-  it('returns 200 when given 200', () => {
-    expect(coinchanger.changeCoins(200)).toEqual(200);
-    });
-
-  [200, 100, 50, 20, 10, 5, 2, 1].forEach(coin => {
-    expect(coinchanger.changeCoins(coin)).toEqual(coin);
-    });
-
-  it('returns 2, 1  when given 3', () => {
-    expect(coinchanger.changeCoins(3)).toEqual([2, 1]);
-    });
+    it('returns "not a coin" when given 6', () => {
+      expect(coinchanger.changeCoins(6)).toEqual('not a coin');
+      });
   });
 });
--- src/coinchanger.js
+++ src/coinchanger.js
@@ -1,9 +1,20 @@
 function changeCoins(money) {
-  if(money === 3) {
-    return[2, 1]
-  } else {
+  const possibleChange = [200, 100, 50, 20, 10, 5, 2, 1];
+
+  for(let i = 0; i < possibleChange.length; i++) {
+    if(possibleChange[i] === money) {
       return money;
+    }
   }
+  
+  if(money === 3) {
+    return [2, 1];
+  } else {
+    return('not a coin');
+  }
+
+
+
 };
 
 module.exports = { changeCoins };
